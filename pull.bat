7522796e0d55fa6777e53b01ec6e4a4dfe79506c
Thomas C. Hottinger <tomhottinger+leanpub@gmail.com>
2016-05-10 11:52:58 +0200

    rewriting history

---
+++ pull.bat
@@ -0,0 +1,4 @@
+set http_proxy=http://hottinth:alibaba999@proxy.gsdnet.ch:8080
+set https_proxy=https://hottinth:alibaba999@proxy.gsdnet.ch:8080
+set GIT_SSL_NO_VERIFY=true
+git pull 
---
+++ push.bat
@@ -0,0 +1,6 @@
+set http_proxy=http://hottinth:alibaba999@proxy.gsdnet.ch:8080
+set https_proxy=https://hottinth:alibaba999@proxy.gsdnet.ch:8080
+set GIT_SSL_NO_VERIFY=true
+git add . 
+git commit
+git push origin master
